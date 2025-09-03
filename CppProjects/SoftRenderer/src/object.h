e35207c68540c955ecaf167b74642fe04d3d72ed
MAXISTART <984402338@qq.com>
2025-09-04 01:34:14 +0800

    ~

--- CppProjects/SoftRenderer/src/object.h
+++ CppProjects/SoftRenderer/src/object.h
@@ -1,2 +1,17 @@
-// world matrix
-// vertex
+#include "Eigen/Eigen"
+
+
+class Object
+{
+public:
+	Object( const std::vector<Eigen::Vector4f>& Vertexes, const Eigen::Vector4f& Position, const Eigen::Vector4f& Scale );
+
+	std::vector<Eigen::Vector4f>& GetVertexes() const;
+	Eigen::Matrix4f GetWorldMatrix() const;
+
+private:
+
+	std::vector<Eigen::Vector4f> mVertexes;
+	Eigen::Vector4f mPosition;
+	Eigen::Vector4f mScale;
+};
